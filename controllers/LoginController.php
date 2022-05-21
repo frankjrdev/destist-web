0fd3f7ca88d07d1576d85f87c50e228a84593ffc
frankjrdev <frankjrdev@gmail.com>
2022-05-21 17:22:31 -0400

    Realizada la pagina de loggin con sus estilos

---
+++ controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController
+{
+    public static function login(Router $router)
+    {
+        $router->render('auth/login');
+    }
+
+    public static function logout()
+    {
+        echo "Desde el logout";
+    }
+
+    public static function olvide()
+    {
+        echo "Desde olvide";
+    }
+
+    public static function recuperar()
+    {
+        echo "Desde recuperar";
+    }
+
+    public static function crear()
+    {
+        echo "Desde crear";
+    }
+
+}
